f65aee33b51824a1036b3b5444a992d0c88b0593
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2025-02-13 16:22:40 +0530

    Add TextPressure component and jsrepo configuration; update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import TextPressure from "@/lib/blocks/text/TextPressure/TextPressure";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      supp nigga
-     </div>
+    <div className="h-[100vh] w-full flex items-center justify-center">
+      <TextPressure text="Vortex" minFontSize={300} />
+    </div>
   );
 }
